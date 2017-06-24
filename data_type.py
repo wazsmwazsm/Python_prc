b8823c609f7fbe5077d97f272c379b00a415114d
MrQin <18748406022@163.com>
2017-06-24 23:43:25 +0800

    Data type

---
+++ data_type.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+print('''line1
+... line2
+... line3''')
+
+
+# 地板除
+
+print(4//3);
+
+print(True and False)
+print(not False)
+print(None)
+
+# 转义不转义
+# python "" '' 没区别
+print('hello \'sss\'')
+print(r'hello \'sss\'') # 不转义原样输出
