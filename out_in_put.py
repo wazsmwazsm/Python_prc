dfb35c351a470fc2d1397dcb980a5a2989376f1b
MrQin <18748406022@163.com>
2017-06-24 23:29:42 +0800

    Input Output

---
+++ out_in_put.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+# 都好会变为空格分隔
+print("Hello", "Mr qin", "good morning!")
+
+# 输出中文, windows git bash 设置为 GBK
+print("秦先生你好!")
+
+# 输入
+name = input('输入你的名字:')
+
+print('hello', name)
