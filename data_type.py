ed1d748173628a1dc08fe61984859306a5872645
wazsmwazsm <18748406022@163.com>
2017-06-28 10:34:32 +0800

    update data_type

--- data_type.py
+++ data_type.py
@@ -2,7 +2,6 @@
 # -*- coding:UTF-8 -*-
 
 
-
 # 字符串
 
 word = '字符串'
@@ -12,10 +11,101 @@ paragraph = """这是一个段落，
 
 print(word, sentence, paragraph)
 
-
-
-
 # 转义不转义
 # python "" '' 没区别
 print('hello \'sss\'')
 print(r'hello \'sss\'') # 不转义原样输出
+
+str = 'Runoob'
+
+print (str)          # 输出字符串
+print (str[0:-1])    # 输出第一个到倒数第二个的所有字符
+print (str[0])       # 输出字符串第一个字符
+print (str[2:5])     # 输出从第三个开始到第五个的字符(包含 5)
+print (str[2:])      # 输出从第三个开始的后的所有字符
+print (str * 2)      # 输出字符串两次
+print (str + "TEST") # 连接字符串
+
+word = 'Python'
+print(word[0], word[5])
+print(word[-1], word[-6])
+
+# number
+
+a, b, c, d = 20, 5.5, True, 4+3j
+
+print(a, b, c, d)
+print(type(a), type(b), type(c), type(d))
+print(isinstance(a, int))
+print(type(type))
+
+# list
+list = [ 'abcd', 786 , 2.23, 'runoob', 70.2 ]
+tinylist = [123, 'runoob']
+
+print (list)            # 输出完整列表
+print (list[0])         # 输出列表第一个元素
+print (list[1:3])       # 从第二个开始输出到第三个元素
+print (list[2:])        # 输出从第三个元素开始的所有元素
+print (tinylist * 2)    # 输出两次列表
+print (list + tinylist) # 连接列表
+
+# tuple
+tuple = ( 'abcd', 786 , 2.23, 'runoob', 70.2  )
+tinytuple = (123, 'runoob')
+
+print (tuple)             # 输出完整元组
+print (tuple[0])          # 输出元组的第一个元素
+print (tuple[1:3])        # 输出从第二个元素开始到第三个元素
+print (tuple[2:])         # 输出从第三个元素开始的所有元素
+print (tinytuple * 2)     # 输出两次元组
+print (tuple + tinytuple) # 连接元组
+
+# set
+student = {'Tom', 'Jim', 'Mary', 'Tom', 'Jack', 'Rose'}
+
+print(student)   # 输出集合，重复的元素被自动去掉
+
+# 成员测试
+if('Rose' in student) :
+    print('Rose 在集合中')
+else :
+    print('Rose 不在集合中')
+
+
+# set可以进行集合运算
+a = set('abracadabra')
+b = set('alacazam')
+
+print(a)
+
+print(a - b)     # a和b的差集
+
+print(a | b)     # a和b的并集
+
+print(a & b)     # a和b的交集
+
+print(a ^ b)     # a和b中不同时存在的元素
+
+
+# dict
+dict = {}
+dict['one'] = "1 - 菜鸟教程"
+dict[2]     = "2 - 菜鸟工具"
+
+tinydict = {'name': 'runoob','code':1, 'site': 'www.runoob.com'}
+
+
+print (dict['one'])       # 输出键为 'one' 的值
+print (dict[2])           # 输出键为 2 的值
+print (tinydict)          # 输出完整的字典
+print (tinydict.keys())   # 输出所有键
+print (tinydict.values()) # 输出所有值
+
+# 遍历来创建 dict
+a = {x: x**2 for x in (2, 4, 6)}
+print(a)
+
+# 注意 遍历 dict 的话，默认遍历的是键值
+for x in a:
+    print(x, a[x])
--- operator.py
+++ operator.py
@@ -6,3 +6,6 @@ print(4//3);
 print(True and False)
 print(not False)
 print(None)
+
+# 开方
+print(2 ** 5)
