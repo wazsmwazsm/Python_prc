a6d174fcee1ada7467a97f36af6d810f16cc9b71
MrQin <18748406022@163.com>
2017-07-02 11:29:14 +0800

    Update tuple

--- tuple.py
+++ tuple.py
@@ -13,3 +13,18 @@ t = ('a', 'b', ['A', 'B'])
 t[2][0] = 'X'
 t[2][1] = 'Y'
 print(t)
+
+# 合并
+tup1 = (12, 34.56);
+tup2 = ('abc', 'xyz')
+
+print(tup1 + tup2)
+
+del tup1
+# print(tup1)
+# 重复
+print(tup2 * 3)
+
+tp = (1,2,4,5,66,3)
+# 内置函数
+print(len(tp), max(tp), min(tp))
