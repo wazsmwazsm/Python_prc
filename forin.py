e13d43b72194b490cdb0b12e8dcc2088926a8644
wazsmwazsm <18748406022@163.com>
2017-07-03 10:05:52 +0800

    update for in

--- forin.py
+++ forin.py
@@ -8,3 +8,24 @@ for name in names:
 
 for x in range(1,40,2):
     print(x)
+
+# for in else
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, '等于', x, '*', n//x)
+    else:
+        # 循环中没有找到元素
+        print(n, ' 是质数')
+
+# 对字符串进行循环 python 中字符串相当于一个 list
+for letter in 'Runoob':
+    if letter == 'o':
+        continue
+    print('当前字母 : ', letter)
+
+# 模仿 for 循环
+a = ['Google', 'Baidu', 'Runoob', 'Taobao', 'QQ']
+for i in range(len(a)):
+    print(i, a[i])
