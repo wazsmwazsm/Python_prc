6de8db149ab62fd9c7eb76b442a213cfaa621242
wazsmwazsm <18748406022@163.com>
2017-08-01 10:13:17 +0800

    json

---
+++ json_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+import json
+
+# Python 字典类型转换为 JSON 对象
+data = {
+    'no' : 1,
+    'name' : 'Runoob',
+    'url' : 'http://www.runoob.com'
+}
+
+json_str = json.dumps(data)
+
+print ("Python 原始数据：", repr(data))
+print ("JSON 对象：", json_str)
+
+# 将 JSON 对象转换为 Python 字典
+data2 = json.loads(json_str)
+print ("data2['name']: ", data2['name'])
+print ("data2['url']: ", data2['url'])
